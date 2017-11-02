dd9580161a0599d31eaed2e0d741e41bb9b2e7ab
Gustavo Amorim <gustavo.lonfinwe@gmail.com>
2017-11-02 20:51:49 -0200

    CRUD tabela disciplina

--- Projeto/Projeto/P.Interface/WindowsForms/P.WindowsForms/Formularios/FormDisciplina.Designer.cs
+++ Projeto/Projeto/P.Interface/WindowsForms/P.WindowsForms/Formularios/FormDisciplina.Designer.cs
@@ -35,6 +35,7 @@
             this.dataGridDisciplina = new System.Windows.Forms.DataGridView();
             this.btnSalvarDisciplina = new System.Windows.Forms.Button();
             this.btnEditarDisciplina = new System.Windows.Forms.Button();
+            this.btnExcluirDisc = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridDisciplina)).BeginInit();
             this.SuspendLayout();
             // 
@@ -78,6 +79,7 @@
             this.dataGridDisciplina.Name = "dataGridDisciplina";
             this.dataGridDisciplina.Size = new System.Drawing.Size(914, 351);
             this.dataGridDisciplina.TabIndex = 4;
+            this.dataGridDisciplina.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridDisciplina_CellContentClick);
             // 
             // btnSalvarDisciplina
             // 
@@ -87,21 +89,34 @@
             this.btnSalvarDisciplina.TabIndex = 5;
             this.btnSalvarDisciplina.Text = "Salvar";
             this.btnSalvarDisciplina.UseVisualStyleBackColor = true;
+            this.btnSalvarDisciplina.Click += new System.EventHandler(this.btnSalvarDisciplina_Click);
             // 
             // btnEditarDisciplina
             // 
-            this.btnEditarDisciplina.Location = new System.Drawing.Point(226, 139);
+            this.btnEditarDisciplina.Location = new System.Drawing.Point(241, 139);
             this.btnEditarDisciplina.Name = "btnEditarDisciplina";
             this.btnEditarDisciplina.Size = new System.Drawing.Size(105, 28);
             this.btnEditarDisciplina.TabIndex = 6;
             this.btnEditarDisciplina.Text = "Editar";
             this.btnEditarDisciplina.UseVisualStyleBackColor = true;
+            this.btnEditarDisciplina.Click += new System.EventHandler(this.btnEditarDisciplina_Click);
+            // 
+            // btnExcluirDisc
+            // 
+            this.btnExcluirDisc.Location = new System.Drawing.Point(418, 139);
+            this.btnExcluirDisc.Name = "btnExcluirDisc";
+            this.btnExcluirDisc.Size = new System.Drawing.Size(105, 28);
+            this.btnExcluirDisc.TabIndex = 7;
+            this.btnExcluirDisc.Text = "Excluir";
+            this.btnExcluirDisc.UseVisualStyleBackColor = true;
+            this.btnExcluirDisc.Click += new System.EventHandler(this.btnExcluirDisc_Click);
             // 
             // FormDisciplina
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1269, 661);
+            this.Controls.Add(this.btnExcluirDisc);
             this.Controls.Add(this.btnEditarDisciplina);
             this.Controls.Add(this.btnSalvarDisciplina);
             this.Controls.Add(this.dataGridDisciplina);
@@ -127,5 +142,6 @@
         private System.Windows.Forms.DataGridView dataGridDisciplina;
         private System.Windows.Forms.Button btnSalvarDisciplina;
         private System.Windows.Forms.Button btnEditarDisciplina;
+        private System.Windows.Forms.Button btnExcluirDisc;
     }
 }
--- Projeto/Projeto/P.Interface/WindowsForms/P.WindowsForms/Formularios/FormDisciplina.cs
+++ Projeto/Projeto/P.Interface/WindowsForms/P.WindowsForms/Formularios/FormDisciplina.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using P.Modelo;
+using System.Data.Entity;
+
 
 namespace P.WindowsForms.Formularios
 {
     public partial class FormDisciplina : Form
     {
+        faculdadeEntities db = new faculdadeEntities();
+        private Disciplina disciplina;
+
         public FormDisciplina()
         {
             InitializeComponent();
@@ -39,7 +45,108 @@ namespace P.WindowsForms.Formularios
             dataGridDisciplina.Columns[2].DataPropertyName = "Professor";
             dataGridDisciplina.Columns[2].Width = 150;
             dataGridDisciplina.Columns[2].Name = "Professor";
+
+            carregarGrid();
         }
 
+        public void carregarGrid()
+        {
+            var disciplinas = from disciplina in db.Disciplina
+                              select (new
+                              {
+                                  disciplina.idDisciplina,
+                                  disciplina.descricao,
+                                  disciplina.Professor
+                              });
+
+
+            dataGridDisciplina.DataSource = disciplinas.ToList();
+        }
+
+        private void btnSalvarDisciplina_Click(object sender, EventArgs e)
+        {
+            if (disciplina == null)
+                disciplina = new Disciplina();
+
+            int IdDisciplina = disciplina.idDisciplina > 0 ? disciplina.idDisciplina : 0;
+
+            disciplina.idDisciplina = IdDisciplina;
+            disciplina.descricao = textBoxNomeDisciplina.Text;
+
+            string mensagem = "";
+
+            if (disciplina.idDisciplina == 0)
+            {
+                db.Disciplina.Add(disciplina);
+                mensagem = "Disciplina cadastrada com sucesso!!";
+            }
+
+            else
+            {
+                var obj = db.Entry(disciplina);
+                obj.Property(x => x.descricao).IsModified = true;
+
+                mensagem = "Disciplina alterada com sucesso!!";
+            }
+
+            db.SaveChanges();
+
+            MessageBox.Show(mensagem);
+            carregarGrid();
+            limparCampos();
+        }
+
+        public void limparCampos() {
+            textBoxNomeDisciplina.Clear();
+            disciplina = null;
+
+        }
+
+        private void btnEditarDisciplina_Click(object sender, EventArgs e)
+        {
+            if (dataGridDisciplina.SelectedRows.Count > 0)
+            {
+                DataGridViewRow linha = dataGridDisciplina.SelectedRows[0];
+                int IdDisciplina = (int)linha.Cells["idDisciplina"].Value;
+
+                disciplina = db.Disciplina.Where(x => x.idDisciplina == IdDisciplina).FirstOrDefault();
+                textBoxNomeDisciplina.Text = disciplina.descricao;
+            }
+
+            else
+            {
+                MessageBox.Show("Selecione um disciplina!");
+            }
+
+        }
+    private void dataGridDisciplina_CellContentClick(object sender, DataGridViewCellEventArgs e)
+    {
+
     }
+
+        private void btnExcluirDisc_Click(object sender, EventArgs e)
+        {
+            if (dataGridDisciplina.SelectedRows.Count > 0)
+            {
+                DataGridViewRow linha = dataGridDisciplina.SelectedRows[0];
+                int IdDisciplina = (int)linha.Cells["idDisciplina"].Value;
+
+                disciplina = db.Disciplina.Where(x => x.idDisciplina == IdDisciplina).FirstOrDefault();
+
+                db.Disciplina.Remove(disciplina);
+                db.SaveChanges();
+
+                MessageBox.Show("Disciplina excluída com sucesso!");
+                carregarGrid();
+                limparCampos();
+            }
+
+            else
+            {
+                MessageBox.Show("Selecione uma disciplina para ser excluída");
+            }
+        }
+    }
+   
 }
+
